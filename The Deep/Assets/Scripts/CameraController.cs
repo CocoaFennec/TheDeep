0247b4b12605921fb7fb32a06c49c2424fa80290
Cocoa <cocoafennec@outlook.com>
2019-06-25 22:01:44 +0100

    Simple Camera Control
    Created simple camera control system to track player sprite

---
+++ The Deep/Assets/Scripts/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    GameObject camera;
+    Transform cameraTransform;
+    Transform transform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera = GameObject.FindGameObjectWithTag("MainCamera");
+        cameraTransform = camera.GetComponent<Transform>();
+        transform = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cameraTransform.position = new Vector3(transform.position.x, transform.position.y, -10);
+    }
+}
